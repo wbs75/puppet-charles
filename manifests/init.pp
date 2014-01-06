162905ffdf4405f00173dbd4c58eac01237a8893
Katsuma Ito <katsumai@gmail.com>
2014-01-06 11:02:30 +0900

    cdn2.charlesprox.com isn't reachable

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class charles {
   package { 'Charles':
     provider => 'appdmg_eula_charles',
-    source   => 'http://cdn2.charlesproxy.com/release/3.8.3/charles-proxy-3.8.3a-applejava.dmg',
+    source   => 'http://www.charlesproxy.com/assets/release/3.8.3/charles-proxy-3.8.3a-applejava.dmg',
   }
 }
