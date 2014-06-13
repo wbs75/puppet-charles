a5651f12f4231d59ffb6cc964a05cabab81c9fd2
wbs75 <wbs75@me.com>
2014-06-13 10:31:29 -0600

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class charles {
   package { 'Charles':
     provider => 'appdmg_eula_charles',
-    source   => 'http://www.charlesproxy.com/assets/release/3.8.3/charles-proxy-3.8.3a-applejava.dmg',
+    source   => 'http://www.charlesproxy.com/assets/release/3.9.2/charles-proxy-3.9.2-applejava.dmg',
   }
 }
