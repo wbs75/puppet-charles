d2ccbd15583f7c2a51aeff4469a7d6f1cb6a0c0a
wbs75 <wbs75@me.com>
2014-06-13 10:32:26 -0600

    Update charles_spec.rb

--- spec/classes/charles_spec.rb
+++ spec/classes/charles_spec.rb
@@ -4,6 +4,6 @@ describe 'charles' do
 
   it { should contain_class('charles') }
   it { should contain_package('Charles').with_provider('appdmg_eula_charles') }
-  it { should contain_package('Charles').with_source('http://www.charlesproxy.com/assets/release/3.8.3/charles-proxy-3.8.3a-applejava.dmg') }
+  it { should contain_package('Charles').with_source('http://www.charlesproxy.com/assets/release/3.9.2/charles-proxy-3.9.2-applejava.dmg') }
 
 end
